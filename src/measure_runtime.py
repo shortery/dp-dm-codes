5d169ed2c19c017610d8abef240aae98f70ca1df
Petra Krátká <70586740+shortery@users.noreply.github.com>
2024-02-18 10:37:32 +0100

    improve measure_runtime.py to include all the times and params and save data into file

--- src/measure_runtime.py
+++ src/measure_runtime.py
@@ -1,8 +1,12 @@
+import statistics
+import time
+import timeit
+import json
+
 import typer
 import torch
 import lightning.pytorch as pl
-import time
-import timeit
+import logging
 import onnx
 import onnxruntime
 import numpy as np
@@ -15,35 +19,37 @@ app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_enable=
 
 @app.command()
 def main(
-    batch_size: int,
-    num_batches: int,
+    num_examples: int,
     checkpoint_path: str,
+    saved_runtimes_path: str,
     is_onnx: bool = False,
-    onnx_model_path: str = "exported_onnx_model.onnx",
     run_on_cpu: bool = False,
-    seed: int = 0
+    onnx_model_path: str = "exported_onnx_model.onnx",
+    seed: int = 0,
+    batch_size: int = 1
 ) -> None:
     
+    torch.set_float32_matmul_precision('high')
+    logging.getLogger("lightning.pytorch.accelerators.cuda").setLevel(logging.WARNING)
     torch.manual_seed(seed)
-    random_tensors = torch.rand(size=(batch_size * num_batches, 3, 128, 128))
+    device = 'cpu' if run_on_cpu else 'cuda'
+    random_tensors = torch.rand(size=(num_examples, 3, 128, 128)).to(device)
 
     dataloader_random_tensors = torch.utils.data.DataLoader(
         dataset=[{"image": x} for x in random_tensors],
         batch_size=batch_size
     )
 
-    trainer = pl.Trainer()
-    if run_on_cpu:
-        loaded_model = my_training.LitAutoEncoder.load_from_checkpoint(checkpoint_path, map_location=torch.device("cpu"))
-    else:
-        loaded_model = my_training.LitAutoEncoder.load_from_checkpoint(checkpoint_path)
+    loaded_model = my_training.LitAutoEncoder.load_from_checkpoint(checkpoint_path, map_location=device)
     loaded_model.eval()
 
+    list_wall_times = []
+    list_process_times = []
 
     if is_onnx:
         torch.onnx.export(
             loaded_model,
-            torch.rand(size=(batch_size, 3, 128, 128)),
+            torch.rand(size=(batch_size, 3, 128, 128)).to(device),
             onnx_model_path,
             input_names=["image"],
             output_names = ['output'],
@@ -52,37 +58,60 @@ def main(
 
         onnx_model = onnx.load(onnx_model_path)
         onnx.checker.check_model(onnx_model)
-        if run_on_cpu:
-            onnx_inference_session = onnxruntime.InferenceSession(onnx_model_path, providers=["CPUExecutionProvider"])
-        else:
-            onnx_inference_session = onnxruntime.InferenceSession(onnx_model_path, providers=["CUDAExecutionProvider"])
 
-        random_arrays = pd.DataFrame(dataloader_random_tensors).map(np.asarray).to_dict('records')
+        providers=["CPUExecutionProvider"] if run_on_cpu else ["CUDAExecutionProvider", "CPUExecutionProvider"]
+        onnx_inference_session = onnxruntime.InferenceSession(onnx_model_path, providers=providers)
 
-        start_wall_time = timeit.default_timer()
-        start_cpu_time = time.process_time()
+        random_arrays = pd.DataFrame(dataloader_random_tensors).map(lambda x: onnxruntime.OrtValue.ortvalue_from_numpy(x.cpu().numpy(), device, 0)).to_dict('records')
+
+        onnx_inference_session.run(None, random_arrays[0]) # first slow prediction
 
         for random_array in tqdm.tqdm(random_arrays):
+            start_wall_time = timeit.default_timer()
+            start_process_time = time.process_time()
+
             onnx_inference_session.run(None, random_array)
 
-        end_wall_time = timeit.default_timer()
-        end_cpu_time = time.process_time()
+            end_wall_time = timeit.default_timer()
+            end_process_time = time.process_time()
+
+            list_wall_times.append(round(end_wall_time - start_wall_time, 6))
+            list_process_times.append(round(end_process_time - start_process_time, 6))
 
 
     else:
-        start_wall_time = timeit.default_timer()
-        start_cpu_time = time.process_time()
 
-        trainer.predict(loaded_model, dataloader_random_tensors)
+        loaded_model(next(iter(dataloader_random_tensors))["image"]) # first slow prediction
 
-        end_wall_time = timeit.default_timer()
-        end_cpu_time = time.process_time()
+        for batch in tqdm.tqdm(dataloader_random_tensors):
+            start_wall_time = timeit.default_timer()
+            start_process_time = time.process_time()
 
-    print()
-    print(checkpoint_path)
-    print("wall time:", round(end_wall_time - start_wall_time, 4), "seconds")
-    print("cpu time:", round(end_cpu_time - start_cpu_time, 4), "seconds")
-    print()
+            loaded_model(batch["image"]) # predict
+
+            end_wall_time = timeit.default_timer()
+            end_process_time = time.process_time()
+
+            list_wall_times.append(round(end_wall_time - start_wall_time, 6))
+            list_process_times.append(round(end_process_time - start_process_time, 6))
+
+
+    runtimes_dict = {
+        "checkpoint_path": checkpoint_path,
+        "num_examples": num_examples,
+        "is_onnx": is_onnx,
+        "run_on_cpu": run_on_cpu,
+        "wall_time": list_wall_times,
+        "process_time": list_process_times,
+        "median_wall_time": round(statistics.median(list_wall_times), 6),
+        "median_process_time": round(statistics.median(list_process_times), 6),
+        "loaded_model_hparams": loaded_model.hparams
+    }
+    # "a" means append to file (write just at the end)
+    with open(saved_runtimes_path, "a") as file:
+        json.dump(runtimes_dict, file)
+        file.write("\n")
+
 
 
 if __name__ == "__main__":
@@ -90,4 +119,4 @@ if __name__ == "__main__":
 
 
 # run in terminal:
-# $ for ckpt in $(cat checkpoints.txt); do python src/measure_runtime.py 32 100 $ckpt; done
+# $ for ckpt in $(cat checkpoints_opt_on_synth.txt); do python src/measure_runtime.py 100 $ckpt "checkpoints_opt_on_synth_runtimes.jsonl"; sleep 2; done
