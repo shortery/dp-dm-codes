b111516b3dbc9e917f0e679039305709fc8eea07
Petra Krátká <70586740+shortery@users.noreply.github.com>
2024-04-07 22:10:35 +0200

    add measure_accuracy.py

---
+++ src/measure_accuracy.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from PIL import Image
+import datasets
+import lightning.pytorch as pl
+import torch
+import itertools
+from tqdm import tqdm
+import random
+import typer
+import json
+
+import my_utils
+import my_training
+import my_datasets
+
+app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_enable=False)
+
+def prepare_test_dataset(example: dict, random_generator:random.Random) -> dict:
+    image: Image.Image = example["image"]
+    image = image.resize((128, 128), resample=Image.Resampling.NEAREST)
+    angle = random_generator.randint(0, 3) * 90
+    image = image.rotate(angle)
+    return {"image": image}
+
+def preprocess_image(batch):
+    return {"image": my_datasets._preprocess(np.asarray(batch["image"]))}
+
+@app.command()
+def main(
+    checkpoint_path: str,
+    saved_accuracies_path: str,
+    seed: int = 0,
+) -> None:
+    
+    # load dataset from hugging face
+    hf_test_dataset = datasets.load_dataset("shortery/dm-codes")["test"]
+
+    # initiate random generator
+    random_generator = random.Random(seed)
+
+    # crop and preprocess the image
+    hf_dataset = hf_test_dataset.map(my_datasets.crop_dm_code)
+    hf_dataset = hf_dataset.map(lambda x: prepare_test_dataset(x, random_generator))
+
+    # create pandas dataframe and add new collumn decoded_text
+    pd_dataset = pd.DataFrame(hf_dataset)
+    np_grayscale_images = pd_dataset["image"].map(lambda x: np.asarray(x.convert("L")))
+    pd_dataset["decoded_text_before_nn"] = np_grayscale_images.map(lambda x: my_utils.decode_dm_code(x))
+    
+    # prepare dataset to be an input to the network
+    preprocessed_dataset = hf_dataset.map(preprocess_image)
+
+    # create dataloader
+    dataloader_test = torch.utils.data.DataLoader(
+        dataset=preprocessed_dataset.with_format("torch"),
+        batch_size=64
+    )
+
+    # load model from checkpoint and compute predictions
+    trainer = pl.Trainer(precision=16)
+    loaded_model = my_training.LitAutoEncoder.load_from_checkpoint(checkpoint_path)
+    loaded_model.eval()
+    predictions = trainer.predict(loaded_model, dataloader_test)
+
+    # chain all predictions together to get one list
+    # (otherwise I could iterate it as "for batch in predictions: for pred in batch: ...")
+    pd_dataset["nn_prediction"] = list(itertools.chain(*predictions))
+
+    np_prediction_for_image = pd_dataset["nn_prediction"].map(lambda x: np.squeeze(my_utils.tensor_to_numpy_for_image(x.unsqueeze(dim=0))))
+    pd_dataset["nn_output_image"] = np_prediction_for_image.map(lambda x: Image.fromarray(x, mode="L"))
+    pd_dataset["decoded_text_after_nn"] = np_prediction_for_image.map(my_utils.decode_dm_code)
+
+    accuracies_dict = {
+        "checkpoint_path": checkpoint_path,
+        "architecture": loaded_model.hparams["architecture_config"]["arch"],
+        "encoder": loaded_model.hparams["architecture_config"]["encoder_name"],
+    }
+
+    num_all = len(pd_dataset)
+    for decoded_text_column in ["decoded_text_before_nn", "decoded_text_after_nn"]:
+        num_correctly_decoded = len(pd_dataset[(pd_dataset[decoded_text_column] == pd_dataset["text"])])
+        num_decoded = len(pd_dataset[(~pd_dataset[decoded_text_column].isna())])
+        if decoded_text_column == "decoded_text_before_nn":
+            key_string = "before_nn_"
+        elif decoded_text_column == "decoded_text_after_nn":
+            key_string = "after_nn_"
+        accuracies_dict[key_string + "decode_rate"] = np.round(num_correctly_decoded / num_all, 4)
+        accuracies_dict[key_string + "misread_rate"] = np.round((num_decoded - num_correctly_decoded) / num_all, 4)
+
+    
+    # "a" means append to file (write just at the end)
+    with open(saved_accuracies_path, "a") as file:
+        json.dump(accuracies_dict, file)
+        file.write("\n")
+
+
+
+if __name__ == "__main__":
+    app()
+
+
+# run in terminal:
+# $ for ckpt in $(cat checkpoints_opt_on_real.txt); do python src/measure_accuracy.py $ckpt "checkpoints_opt_on_real_accuracies.jsonl"; done
