68bf0c04417d83c92110d1baf2241d6ba6f6d45b
Petra Krátká <70586740+shortery@users.noreply.github.com>
2026-01-01 20:50:38 +0100

    add parameters to training script and create data script

--- dm_codes/scripts/create_synthetic_dataset.py
+++ dm_codes/scripts/create_synthetic_dataset.py
@@ -1,24 +1,33 @@
 from PIL import Image
 import os
+import typer
 
 import dm_codes.datasets 
 
-valid_size = 100
-valid_seed = 0
+app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_enable=False)
 
-pd_dataset = dm_codes.datasets.create_dataset(valid_size, valid_seed)
+@app.command()
+def main(
+    image_dir: str = "datasets/synthetic_valid_dataset",
+    dataset_size: int = 100,
+    dataset_seed: int = 0,
+) -> None:
 
-image_dir = "../../datasets/synthetic_valid_dataset_3"
-os.makedirs(image_dir, exist_ok=False)
-for col in pd_dataset.columns:
-    os.makedirs(f"{image_dir}/{col}", exist_ok=False)
+    pd_dataset = dm_codes.datasets.create_dataset(dataset_size, dataset_seed)
 
-for i in range(valid_size):
+    os.makedirs(image_dir, exist_ok=False)
     for col in pd_dataset.columns:
-        file_name = f"{image_dir}/{col}/{i}"
-        if col == "text":
-            with open(file_name + ".txt", "w") as file:
-                file.write(pd_dataset[col][i].decode("utf8"))
-        else:
-            Image.fromarray(pd_dataset[col][i]).save(file_name + ".png")
+        os.makedirs(f"{image_dir}/{col}", exist_ok=False)
+
+    for i in range(dataset_size):
+        for col in pd_dataset.columns:
+            file_name = f"{image_dir}/{col}/{i}"
+            if col == "text":
+                with open(file_name + ".txt", "w") as file:
+                    file.write(pd_dataset[col][i].decode("utf8"))
+            else:
+                Image.fromarray(pd_dataset[col][i]).save(file_name + ".png")
             
+
+if __name__ == "__main__":
+    app()
--- dm_codes/scripts/train.py
+++ dm_codes/scripts/train.py
@@ -10,6 +10,7 @@ import wandb
 import wandb.sdk
 import datasets
 import tqdm
+import typer
 
 import dm_codes.datamatrix_provider
 import dm_codes.datasets 
@@ -17,119 +18,132 @@ import dm_codes.training
 import dm_codes.callbacks
 import dm_codes.utils
 
-with open("../../config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_enable=False)
 
-train_dataloader = torch.utils.data.DataLoader(
-    dataset=dm_codes.datasets.DMIterableDataset(dm_codes.datamatrix_provider.DataMatrixProvider()),
-    batch_size=config["train_batch_size"]
-)
+@app.command()
+def main(
+    config_path: str = "config.yaml",
+    synthetic_valid_dataset_path: str = "datasets/synthetic_valid_dataset",
+    checkpoints_path: str = "checkpoints",
+    log_n_predictions: int = 36,
+) -> None:
 
-synthetic_valid_dataset = dm_codes.datasets.DMMapDatasetFromFolder(folder="../../datasets/synthetic_valid_dataset_3")
-synthetic_valid_dataloader = torch.utils.data.DataLoader(
-    dataset=synthetic_valid_dataset,
-    batch_size=config["valid_batch_size"]
-)
-fst_synthetic_batch = next(iter(synthetic_valid_dataloader)) 
-print("synthetic min max:", fst_synthetic_batch["corrupted"].min(), fst_synthetic_batch["corrupted"].max())
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
 
-hf_valid_dataset = datasets.load_dataset("shortery/dm-codes")["validation"]
-real_valid_dataset = dm_codes.datasets.DMMapDatasetFromHuggingFace(hf_dataset=hf_valid_dataset.map(dm_codes.datasets.crop_dm_code))
-real_valid_dataloader = torch.utils.data.DataLoader(
-    dataset=real_valid_dataset,
-    batch_size=config["valid_batch_size"]
-)
-fst_real_batch = next(iter(real_valid_dataloader)) 
-print("real min max:", fst_real_batch["image"].min(), fst_real_batch["image"].max())
+    train_dataloader = torch.utils.data.DataLoader(
+        dataset=dm_codes.datasets.DMIterableDataset(dm_codes.datamatrix_provider.DataMatrixProvider()),
+        batch_size=config["train_batch_size"]
+    )
 
-os.makedirs("wandb", exist_ok=True)
-wandb_logger = WandbLogger(project="dp-dm-codes", save_dir="wandb")
-wandb_experiment = wandb_logger.experiment
-assert isinstance(wandb_experiment, wandb.sdk.wandb_run.Run)
+    synthetic_valid_dataset = dm_codes.datasets.DMMapDatasetFromFolder(folder=synthetic_valid_dataset_path)
+    synthetic_valid_dataloader = torch.utils.data.DataLoader(
+        dataset=synthetic_valid_dataset,
+        batch_size=config["valid_batch_size"]
+    )
+    fst_synthetic_batch = next(iter(synthetic_valid_dataloader)) 
+    print("synthetic min max:", fst_synthetic_batch["corrupted"].min(), fst_synthetic_batch["corrupted"].max())
 
-wandb_experiment.config.update(config)
-wandb_experiment.define_metric("synthetic_valid/correctly_decoded", summary="max")
-wandb_experiment.define_metric("synthetic_valid/decodable", summary="max")
-wandb_experiment.define_metric("synthetic_valid/mse_loss", summary="min")
-wandb_experiment.define_metric("real_valid/correctly_decoded", summary="max")
-wandb_experiment.define_metric("real_valid/decodable", summary="max")
+    hf_valid_dataset = datasets.load_dataset("shortery/dm-codes")["validation"]
+    real_valid_dataset = dm_codes.datasets.DMMapDatasetFromHuggingFace(hf_dataset=hf_valid_dataset.map(dm_codes.datasets.crop_dm_code))
+    real_valid_dataloader = torch.utils.data.DataLoader(
+        dataset=real_valid_dataset,
+        batch_size=config["valid_batch_size"]
+    )
+    fst_real_batch = next(iter(real_valid_dataloader)) 
+    print("real min max:", fst_real_batch["image"].min(), fst_real_batch["image"].max())
 
+    os.makedirs("wandb", exist_ok=True)
+    wandb_logger = WandbLogger(project="dp-dm-codes", save_dir="wandb")
+    wandb_experiment = wandb_logger.experiment
+    assert isinstance(wandb_experiment, wandb.sdk.wandb_run.Run)
 
-# metrics if the network works perfectly (prediction = target)
-perfect_metrics = []
-for batch in tqdm.tqdm(synthetic_valid_dataloader, desc="computing metrics if prediction=target"):
-    metrics = dm_codes.utils.compute_metrics(target=batch["target"], pred=batch["target"], text=batch["text"], prefix="perfect_network/")
-    perfect_metrics.append(metrics)
-perfect_metrics = pd.DataFrame(perfect_metrics).mean().to_dict()
-print("if prediction=target:")
-print(perfect_metrics)
-
-# metrics if the network just copies input
-# i.e., how many inputs can be (correctly) decoded just by the pylibdmtx code reader
-baseline_metrics = []
-for batch in tqdm.tqdm(synthetic_valid_dataloader, desc="computing metrics if prediction=input"):
-    corrupt = torchvision.transforms.functional.rgb_to_grayscale(batch["corrupted"])
-    metrics = dm_codes.utils.compute_metrics(target=batch["target"], pred=corrupt, text=batch["text"], prefix="copy_baseline/")
-    baseline_metrics.append(metrics)
-baseline_metrics = pd.DataFrame(baseline_metrics).mean().to_dict()
-print("if prediction=input:")
-print(baseline_metrics)
-# now for real dataset
-real_baseline_metrics = []
-for batch in tqdm.tqdm(real_valid_dataloader, desc="computing metrics if prediction=input in real dataset"):
-    image = torchvision.transforms.functional.rgb_to_grayscale(batch["image"])
-    real_metrics = dm_codes.utils.compute_metrics(pred=image, text=batch["text"], prefix="copy_baseline_real/")
-    real_baseline_metrics.append(real_metrics)
-real_baseline_metrics = pd.DataFrame(real_baseline_metrics).mean().to_dict()
-print("if prediction=input in real dataset:")
-print(real_baseline_metrics)
-
-autoencoder = dm_codes.training.LitAutoEncoder(config["architecture"], config["optimizer"])
-
-os.makedirs("../../checkpoints", exist_ok=True)
-checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    dirpath=f"../../checkpoints/{wandb_experiment.name}",
-    filename="step={step}--corr_dec={real_valid/correctly_decoded:.4f}",
-    auto_insert_metric_name=False,
-    save_top_k=2,
-    save_last=True,
-    monitor="real_valid/correctly_decoded",
-    mode="max",
-)
-
-log_n_predictions = 36
-synth_list_idxs = [len(synthetic_valid_dataset)*i//log_n_predictions for i in range(log_n_predictions)]
-real_list_idxs = [len(real_valid_dataset)*i//log_n_predictions for i in range(log_n_predictions)]
-synth_batch_image_idxs = [(i // config["valid_batch_size"], i % config["valid_batch_size"]) for i in synth_list_idxs]
-real_batch_image_idxs = [(i // config["valid_batch_size"], i % config["valid_batch_size"]) for i in real_list_idxs]
-batch_image_idxs = [synth_batch_image_idxs, real_batch_image_idxs]
-image_callback = dm_codes.callbacks.DMPrintingCallback(batch_image_idxs)
-
-early_stop_callback = pl.callbacks.EarlyStopping(**config["early_stopping"])
-
-trainer = pl.Trainer(
-    **config["trainer"],
-    logger=wandb_logger,
-    callbacks=[
-        checkpoint_callback,
-        image_callback,
-        early_stop_callback
-    ]
-)
+    wandb_experiment.config.update(config)
+    wandb_experiment.define_metric("synthetic_valid/correctly_decoded", summary="max")
+    wandb_experiment.define_metric("synthetic_valid/decodable", summary="max")
+    wandb_experiment.define_metric("synthetic_valid/mse_loss", summary="min")
+    wandb_experiment.define_metric("real_valid/correctly_decoded", summary="max")
+    wandb_experiment.define_metric("real_valid/decodable", summary="max")
 
 
-wandb_experiment.log(perfect_metrics, step=0)
-wandb_experiment.log(baseline_metrics, step=0)
-wandb_experiment.log(real_baseline_metrics, step=0)
-wandb_experiment.log({
-    "validation_characteristics":
-    wandb.Table(dataframe=pd.DataFrame([perfect_metrics | baseline_metrics | real_baseline_metrics]))
-}, step=0)
+    # metrics if the network works perfectly (prediction = target)
+    perfect_metrics = []
+    for batch in tqdm.tqdm(synthetic_valid_dataloader, desc="computing metrics if prediction=target"):
+        metrics = dm_codes.utils.compute_metrics(target=batch["target"], pred=batch["target"], text=batch["text"], prefix="perfect_network/")
+        perfect_metrics.append(metrics)
+    perfect_metrics = pd.DataFrame(perfect_metrics).mean().to_dict()
+    print("if prediction=target:")
+    print(perfect_metrics)
 
-trainer.validate(model=autoencoder, dataloaders=[synthetic_valid_dataloader, real_valid_dataloader], verbose=False)
+    # metrics if the network just copies input
+    # i.e., how many inputs can be (correctly) decoded just by the pylibdmtx code reader
+    baseline_metrics = []
+    for batch in tqdm.tqdm(synthetic_valid_dataloader, desc="computing metrics if prediction=input"):
+        corrupt = torchvision.transforms.functional.rgb_to_grayscale(batch["corrupted"])
+        metrics = dm_codes.utils.compute_metrics(target=batch["target"], pred=corrupt, text=batch["text"], prefix="copy_baseline/")
+        baseline_metrics.append(metrics)
+    baseline_metrics = pd.DataFrame(baseline_metrics).mean().to_dict()
+    print("if prediction=input:")
+    print(baseline_metrics)
+    # now for real dataset
+    real_baseline_metrics = []
+    for batch in tqdm.tqdm(real_valid_dataloader, desc="computing metrics if prediction=input in real dataset"):
+        image = torchvision.transforms.functional.rgb_to_grayscale(batch["image"])
+        real_metrics = dm_codes.utils.compute_metrics(pred=image, text=batch["text"], prefix="copy_baseline_real/")
+        real_baseline_metrics.append(real_metrics)
+    real_baseline_metrics = pd.DataFrame(real_baseline_metrics).mean().to_dict()
+    print("if prediction=input in real dataset:")
+    print(real_baseline_metrics)
 
-trainer.fit(
-    model=autoencoder,
-    train_dataloaders=train_dataloader,
-    val_dataloaders=[synthetic_valid_dataloader, real_valid_dataloader],
-)
+    autoencoder = dm_codes.training.LitAutoEncoder(config["architecture"], config["optimizer"])
+
+    os.makedirs(checkpoints_path, exist_ok=True)
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=f"{checkpoints_path}/{wandb_experiment.name}",
+        filename="step={step}--corr_dec={real_valid/correctly_decoded:.4f}",
+        auto_insert_metric_name=False,
+        save_top_k=2,
+        save_last=True,
+        monitor="real_valid/correctly_decoded",
+        mode="max",
+    )
+
+    synth_list_idxs = [len(synthetic_valid_dataset)*i//log_n_predictions for i in range(log_n_predictions)]
+    real_list_idxs = [len(real_valid_dataset)*i//log_n_predictions for i in range(log_n_predictions)]
+    synth_batch_image_idxs = [(i // config["valid_batch_size"], i % config["valid_batch_size"]) for i in synth_list_idxs]
+    real_batch_image_idxs = [(i // config["valid_batch_size"], i % config["valid_batch_size"]) for i in real_list_idxs]
+    batch_image_idxs = [synth_batch_image_idxs, real_batch_image_idxs]
+    image_callback = dm_codes.callbacks.DMPrintingCallback(batch_image_idxs)
+
+    early_stop_callback = pl.callbacks.EarlyStopping(**config["early_stopping"])
+
+    trainer = pl.Trainer(
+        **config["trainer"],
+        logger=wandb_logger,
+        callbacks=[
+            checkpoint_callback,
+            image_callback,
+            early_stop_callback
+        ]
+    )
+
+
+    wandb_experiment.log(perfect_metrics, step=0)
+    wandb_experiment.log(baseline_metrics, step=0)
+    wandb_experiment.log(real_baseline_metrics, step=0)
+    wandb_experiment.log({
+        "validation_characteristics":
+        wandb.Table(dataframe=pd.DataFrame([perfect_metrics | baseline_metrics | real_baseline_metrics]))
+    }, step=0)
+
+    trainer.validate(model=autoencoder, dataloaders=[synthetic_valid_dataloader, real_valid_dataloader], verbose=False)
+
+    trainer.fit(
+        model=autoencoder,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=[synthetic_valid_dataloader, real_valid_dataloader],
+    )
+
+
+if __name__ == "__main__":
+    app()
